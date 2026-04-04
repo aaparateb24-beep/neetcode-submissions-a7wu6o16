f1e91b76b2da729938ec270d34d8992ad59b76f9
Aarya Parate <aaparate_b24@ce.vjti.ac.in>
2026-04-04 23:14:23 +0530

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.py
@@ -0,0 +1,12 @@
+class Solution:
+    def inorderTraversal(self, root):
+        if root is None:
+            return []
+        
+        return (
+            self.inorderTraversal(root.left) +
+            [root.val] +
+            self.inorderTraversal(root.right)
+        )
+    
+
