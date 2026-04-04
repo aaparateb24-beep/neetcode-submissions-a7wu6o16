1dc1e042766e77a52ea9633815adfe2a26120455
Aarya Parate <aaparate_b24@ce.vjti.ac.in>
2026-04-04 23:23:58 +0530

    Add: binary-tree-postorder-traversal - submission-0

---
+++ Data Structures & Algorithms/binary-tree-postorder-traversal/submission-0.py
@@ -0,0 +1,10 @@
+class Solution:
+    def postorderTraversal(self, root):
+        if root is None:
+            return []
+        
+        return (
+            self.postorderTraversal(root.left) +
+            self.postorderTraversal(root.right) +
+            [root.val]
+        )           
