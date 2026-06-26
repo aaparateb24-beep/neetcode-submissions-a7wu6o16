30063790693676ed7d8d6a2adc027a72aa261409
Aarya Parate <aaparate_b24@ce.vjti.ac.in>
2026-06-26 10:57:50 +0530

    Add: asteroid-collision - submission-0

---
+++ Data Structures & Algorithms/asteroid-collision/submission-0.py
@@ -0,0 +1,17 @@
+class Solution:
+    def asteroidCollision(self, asteroids: List[int]) -> List[int]:
+        stack = []
+        for asteroid in asteroids:
+            alive = True 
+            while alive and stack and stack[-1] > 0 and asteroid < 0 :
+                if abs(asteroid ) > stack[-1]       :
+                    stack.pop()
+                    continue
+                elif abs(asteroid) == stack[-1]:
+                    stack.pop()
+                    alive = False
+                else:
+                    alive = False
+            if alive:
+                stack.append(asteroid)
+        return stack                
